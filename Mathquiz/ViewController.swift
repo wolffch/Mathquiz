e06c7be277d4a0c9e123cec5939b48cae723e1e0
Christina Wolff <cwolff@juggernaut.highpoint.edu>
2019-02-18 13:00:50 -0500

    Here JOe

--- Mathquiz/ViewController.swift
+++ Mathquiz/ViewController.swift
@@ -2,24 +2,58 @@
 //  ViewController.swift
 //  Mathquiz
 //
-//  Created by Christina Wolff on 2/18/19.
+//  Created by Christina Wolff and Joe Davidson on 2/18/19.
 //  Copyright © 2019 Christina Wolff. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+        @IBOutlet var NumberOneLabel: UILabel!
+        @IBOutlet var NumberTwoLabel: UILabel!
+        @IBOutlet var operatorsLabel: UILabel!
+        @IBOutlet var pointsLabel: UILabel!
+        @IBOutlet var answerLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+        var points:Int = 0
+    
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            NumberOneLabel.text = number1[currentQuestionIndex]
+            NumberTwoLabel.text = number2[currentQuestionIndex]
+            operatorsLabel.text = operators[currentQuestionIndex]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        }
+    
+    
+        let operators: [String] =    ["+","-","*","/","+","-","*","-","+","-"]
+        let number1: [String] =      ["0","1","2","3","4","5","6","7","8","9"]
+        let number2: [String] =      ["2","7","5","3","8","0","1","4","6","9"]
+        let answers: [String] =      ["2","-6","10","1","12","5","6","3","14","0"]
+        var currentQuestionIndex: Int = 0
+        
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+            var answerAttempt:String
+            answerLabel.text = answerAttempt
+        
+        
+            if answerLabel.text == answers[currentQuestionIndex]{
+                points+=1
+                
+            currentQuestionIndex += 1
+            if currentQuestionIndex == number1.count {
+                currentQuestionIndex = 0
+            }
+                
+                
+            }
 
+        }
+        
 
 }
+
+
 
